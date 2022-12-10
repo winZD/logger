75a233ff564e1f2fabab143a286c09213219a395
winZD <puntica007@gmail.com>
2022-12-10 17:28:33 +0100

    update

--- logger-client/src/components/Login/Login.tsx
+++ logger-client/src/components/Login/Login.tsx
@@ -28,7 +28,7 @@ const Login = (props: PaperProps) => {
       email: (val) => (/^\S+@\S+$/.test(val) ? null : "Invalid email"),
       password: (val) =>
         val.length <= 6
-          ? "Passworddd should include at least 6 characters"
+          ? "Password should include at least 6 characters"
           : null,
     },
   });
