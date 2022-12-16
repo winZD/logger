0abc6bf9d0b9a7a61841fafecb27a676f4488a31
winZD <puntica007@gmail.com>
2022-12-17 00:00:15 +0100

    Implemented react router and app shell

--- logger-client/src/App.tsx
+++ logger-client/src/App.tsx
@@ -1,7 +1,31 @@
 import Login from "./components/Login/Login";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppShellLayout from "./components/AppShellLayout/AppShellLayout";
 
-function App() {
-  return <Login />;
-}
+const App = () => {
+  const router = createBrowserRouter([
+    { path: "/login", element: <Login /> },
+    {
+      path: "/",
+      element: <AppShellLayout />,
+      errorElement: <div>Nothing found</div>,
+      children: [
+        {
+          path: "by-customer",
+          element: <h1>Hello customer</h1>,
+        },
+        {
+          path: "unpaid",
+          element: <h1>Hello unpaid</h1>,
+        },
+      ],
+    },
+  ]);
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+};
 
 export default App;
---
+++ logger-client/src/components/AppShellLayout/AppShellLayout.tsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import {
+  AppShell,
+  Navbar,
+  Header,
+  Text,
+  MediaQuery,
+  Burger,
+  useMantineTheme,
+  NavLink,
+} from "@mantine/core";
+import { Link, Outlet } from "react-router-dom";
+
+const AppShellLayout = () => {
+  const theme = useMantineTheme();
+  const [opened, setOpened] = useState(false);
+  return (
+    <AppShell
+      styles={{
+        main: {
+          background:
+            theme.colorScheme === "dark"
+              ? theme.colors.dark[8]
+              : theme.colors.gray[0],
+        },
+      }}
+      navbarOffsetBreakpoint="sm"
+      asideOffsetBreakpoint="sm"
+      navbar={
+        <Navbar
+          p="md"
+          hiddenBreakpoint="sm"
+          hidden={!opened}
+          width={{ sm: 200, lg: 300 }}
+        >
+          {" "}
+          <Text>Dashboard</Text>
+          <Navbar.Section>
+            <NavLink label="Invoices">
+              <NavLink label="By customer" component={Link} to="by-customer" />
+              <NavLink label="Unpaid" component={Link} to={"unpaid"} />
+            </NavLink>
+          </Navbar.Section>
+        </Navbar>
+      }
+      header={
+        <Header height={50} p="md">
+          <div
+            style={{ display: "flex", alignItems: "center", height: "100%" }}
+          >
+            <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+              <Burger
+                opened={opened}
+                onClick={() => setOpened((o) => !o)}
+                size="sm"
+                color={theme.colors.gray[6]}
+                mr="xl"
+              />
+            </MediaQuery>
+
+            <Text>Application header</Text>
+          </div>
+        </Header>
+      }
+    >
+      <Outlet />
+    </AppShell>
+  );
+};
+
+export default AppShellLayout;
